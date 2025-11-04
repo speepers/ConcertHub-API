bef7132504a10e96aab1ae6ad557e389ddc97a1d
adrian <w0513060@gmail.com>
2025-11-04 12:29:04 -0400

    changed app.js to work with express

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-import http from 'http';
+import express from 'express';
 
-http.createServer(function (req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/html'});
-  res.write('hello world');
-  res.end();
-}).listen(3000);
+const port = process.env.PORT || 3000;
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// routes (api endpoints)
+app.get('/', (req, res) => {
+  res.send('This is the home page!');
+});
+
+app.get('/hello', (req, res) => {
+  res.send('Hello Express!');
+});
+
+app.get('/goodbye', (req, res) => {
+  res.send('Goodbye Express!');
+});
+
+// more routes go here as needed
+
+// start server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
